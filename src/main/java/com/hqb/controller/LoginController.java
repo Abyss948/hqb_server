611afd20ae8da55a79f6732d4047610458dfb16c
yif <1591246866@qq.com>
2020-10-07 23:34:53 +0800

    实现用户注册

--- src/main/java/com/hqb/controller/LoginController.java
+++ src/main/java/com/hqb/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.hqb.pojo.JsonResult;
 import com.hqb.pojo.User;
 import com.hqb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -12,7 +13,7 @@ public class LoginController {
     @Autowired
     UserService userService;
 
-    @PostMapping("/register")
+    @GetMapping("/register")
     public JsonResult<Object> register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email, @RequestParam("wechat") String wechat, @RequestParam("intro") String intro, @RequestParam("question") String question, @RequestParam("answer") String answer,@RequestParam("balance") double balance,@RequestParam("status") int status){
         User user = userService.getUserByUserName(username);
         if (user!=null){
--- src/main/java/com/hqb/controller/UserController.java
+++ src/main/java/com/hqb/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.hqb.controller;
 
 import com.hqb.pojo.JsonResult;
+import com.hqb.pojo.User;
 import com.hqb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,5 +22,15 @@ public class UserController {
         Map<String, Object> map = new HashMap<>();
         map.put("username", userService.getUsernameByUserid(userid));
         return new JsonResult<>(map, "查询成功");
+    }
+
+    @GetMapping("/register")
+    public JsonResult<Object> register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email, @RequestParam("wechat") String wechat, @RequestParam("intro") String intro, @RequestParam("question") String question, @RequestParam("answer") String answer,@RequestParam("balance") double balance,@RequestParam("status") int status){
+        User user = userService.getUserByUserName(username);
+        if (user!=null){
+            return new JsonResult<>("1","用户名重复");
+        }
+        userService.addUser(username,password,email,wechat,intro,question,answer,balance,status);
+        return new JsonResult<>("0","注册成功");
     }
 }
