d0badaaa0d75b585456f29e238bf6acaf2643253
yif <1591246866@qq.com>
2020-10-07 23:23:29 +0800

    创建注册logincontroller，用户添加addUser

--- src/main/java/com/hqb/controller/UserController.java
+++ src/main/java/com/hqb/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.hqb.controller;
 
 import com.hqb.pojo.JsonResult;
+import com.hqb.pojo.User;
 import com.hqb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +23,16 @@ public class UserController {
         Map<String, Object> map = new HashMap<>();
         map.put("username", userService.getUsernameByUserid(userid));
         return new JsonResult<>(map, "查询成功");
+    }
+
+
+    @GetMapping("/register")
+    public JsonResult<Object> register(@RequestParam("username") String username, @RequestParam("password") String password, @RequestParam("email") String email, @RequestParam("wechat") String wechat, @RequestParam("intro") String intro, @RequestParam("question") String question, @RequestParam("answer") String answer,@RequestParam("balance") double balance,@RequestParam("status") int status){
+        User user = userService.getUserByUserName(username);
+        if (user!=null){
+            return new JsonResult<>("1","用户名重复");
+        }
+        userService.addUser(username,password,email,wechat,intro,question,answer,balance,status);
+        return new JsonResult<>("0","注册成功");
     }
 }
