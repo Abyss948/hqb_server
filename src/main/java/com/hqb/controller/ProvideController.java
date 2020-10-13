c021869e97b565db5b84c459d6060458afb76d94
Abyss948 <2292352873@qq.com>
2020-10-13 22:26:57 +0800

    “闲置资金基础”——“贷款计算”按钮

--- src/main/java/com/hqb/controller/ProvideController.java
+++ src/main/java/com/hqb/controller/ProvideController.java
@@ -1,16 +1,47 @@
 package com.hqb.controller;
 
 import com.hqb.pojo.JsonResult;
+import com.hqb.service.AdminService;
+import com.hqb.service.NeedService;
 import com.hqb.service.ProvideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class ProvideController {
     @Autowired
     ProvideService provideService;
+
+    @Autowired
+    NeedService needService;
+
+    @Autowired
+    AdminService adminService;
+
+    @PostMapping("/provideCalculate")
+    public JsonResult<Object> provideCalculate(@RequestParam("goalmoney")double goalmoney,@RequestParam("timelimit")double timelimit,@RequestParam("rate")double rate){
+        if(needService.isOverRate(rate)){
+            Map<String, Object> map1 = new HashMap<>();
+            map1.put("rateMax",adminService.getMaxRate()/100.0);
+            map1.put("rateMin",adminService.getMinRate()/100.0);
+            return new JsonResult<>(map1,"1","汇率超出范围");
+        }
+        Map<String, Object> map = new HashMap<>();
+        map.put("goalmoney",goalmoney);
+        map.put("rate",rate);
+        double interest = goalmoney*rate/100*timelimit;
+        map.put("interest",interest);
+        double servicerate = adminService.getServiceRate();
+        double servicefee = servicerate*goalmoney;
+        map.put("servicefee",servicefee);
+        return new JsonResult<>(map);
+    }
+
 
     @PostMapping("/setNewProvide")
     public JsonResult<Object> setNewProvide(@RequestParam("userid") int userid, @RequestParam("rate") double rate, @RequestParam("timelimit") double timelimit, @RequestParam("goalmoney") double goalmoney) {
