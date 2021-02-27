e5e44cfdbd1fb1cd1c5ff5ab9011ccf3c0ab2a67
Abyss948 <2292352873@qq.com>
2021-02-27 23:43:09 +0800

    MyController

--- src/main/java/com/hqb/controller/MyController.java
+++ src/main/java/com/hqb/controller/MyController.java
@@ -5,6 +5,7 @@ import com.hqb.service.AccountService;
 import com.hqb.service.MyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,15 +17,23 @@ public class MyController {
 
     @Autowired
     AccountService accountService;
-   @Autowired
-   MyService myService;
+    @Autowired
+    MyService myService;
 
     @GetMapping("getMyByUserid")
-    public JsonResult<Map<String, Object>> getMyByUserid(@RequestParam("userid")int userid)
-    {
+    public JsonResult<Map<String, Object>> getMyByUserid(@RequestParam("userid") int userid) {
         Map<String, Object> map = new HashMap<>();
 
-        map.put("My", myService.getMyByUserid(userid));
-        return new JsonResult<>(map,"success");
+        map.put("myinfo", myService.getMyByUserid(userid));
+        return new JsonResult<>(map, "success");
+    }
+
+    @PostMapping("/transferMoney")
+    public JsonResult<Map<String, Object>> transferMoney(@RequestParam("code") String code) {
+        Map<String, Object> map = new HashMap<>();
+        code = "=" + code;
+        System.out.println(code);
+        map.put("code", code);
+        return new JsonResult<>(map);
     }
 }
