a5716068e244cbdc57632c2e797d47789ba90a2b
Abyss948 <2292352873@qq.com>
2021-03-02 21:47:21 +0800

    手续费图

--- src/main/java/com/hqb/controller/WebSerFeeChartController.java
+++ src/main/java/com/hqb/controller/WebSerFeeChartController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -23,7 +24,7 @@ public class WebSerFeeChartController {
         ArrayList<Double> arrayFee = new ArrayList<>();
         for (int i = 0; i < 12; i++) {
             arrayMonth.add(i,i+1);
-            arrayFee.add(i,webSerFeeChartService.getWebSerFeeChart(i+1));
+            arrayFee.add(i,fliter(webSerFeeChartService.getWebSerFeeChart(i+1)));
         }
 
 
@@ -32,5 +33,13 @@ public class WebSerFeeChartController {
         map.put("arrayMonth",arrayMonth);
         map.put("arrayFee",arrayFee);
         return new JsonResult<>(map);
+    }
+
+    public Double fliter(Double d) {
+        if (d == null) {
+            return d;
+        } else {
+            return new BigDecimal(d).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+        }
     }
 }
